c2cdfae17fa12e8b9301064e614feeedaa89bf01
ahmed <ahmed@192.168.1.7>
2020-12-21 22:44:09 +0200

    modify

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/signUp">SignUp</router-link> |
-      <router-link to="/view">View</router-link> 
+      <router-link to="/view">View</router-link>
     </div>
     <router-view />
   </div>
